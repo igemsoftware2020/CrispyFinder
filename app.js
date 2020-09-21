8ca7707249ce8624cc1c999d21e445276f63afe3
관해 (Jack) Park <jkhpark23@utexas.edu>
2020-09-22 01:23:53 +0900

    fixes to get app ready for packaging

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const app = express();
 const scrape = require("./scraper");
 
 
-app.use(express.static("public"));
+app.use(express.static("./resources/app/public"));
 app.set("view engine", "ejs");
+app.set("views", "./resources/app/views");
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+// run electron-packager ./ KSAKorea-Crispy
 
 
 app.get("/", (req, res) => {
